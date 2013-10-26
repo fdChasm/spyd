02e28d0c65552ebd62b0d74a0fcf4ef64259d690
Chasm <fd.chasm@gmail.com>
2013-10-26 06:15:56 -0700

    Make enums return None when you ask for a name or value which doesn't exist.

--- src/cube2common/utils/enum.py
+++ src/cube2common/utils/enum.py
@@ -18,13 +18,13 @@ class enum(object):
             self.__setattr__(item, value)
             
     def by_value(self, value):
-        return self.__by_values[value]
+        return self.__by_values.get(value, None)
     
     def has_value(self, value):
         return value in self.__by_values
     
     def by_name(self, item):
-        return self.__by_names.get(item)
+        return self.__by_names.get(item, None)
     
     def contains(self, item):
         return item in self.__by_names
